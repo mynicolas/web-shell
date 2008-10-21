5828d27a3d4d456bf8c318e1550e3eb6790e73d6
aleksander.adamowski <aleksander.adamowski@4e447baa-4d33-0410-9943-99eb69626ef3>
2008-10-21 21:17:16 +0000

    security fix (issue no. 42)

--- make_certificate.sh
+++ make_certificate.sh
@@ -1,5 +1,3 @@
 #! /bin/sh
-gzip -c --best /var/log/system.log > /tmp/random.dat
-openssl rand -rand file:random.dat 0
-rm random.dat
+umask 077
 openssl req  -config "make_certificate.cfg" -keyout "webshell.pem" -newkey rsa:1024 -nodes -x509 -days 365 -out "webshell.pem"
